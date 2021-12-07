42e6c6359c8158f9f657dcd2427fa9387ffcfe44
Lorenz Ras <lenzras@gmail.com>
2021-12-07 16:05:49 +0900

    Update app.js

--- alternative/app.js
+++ alternative/app.js
@@ -1,8 +1,8 @@
 const line = require('@line/bot-sdk');
 
 const config = {
-  channelAccessToken: 'yTcp2Ki2UhU1rCWRx0dEG0lfdXJzxZqytIutGyaWbTkhaNdFs69dO/f+Vdc2vq9lPpphM7VG1IpbvTGQ+796nG5MV9Ysnya/PIgObkRlTxHgus3A0BzHQ53E+8pWNN4mvndL7B3LLCqR7UKLT+zCzQdB04t89/1O/w1cDnyilFU=',
-  channelSecret: '2c515342b24f76af05471933b56f5d7a',
+  channelAccessToken: 'chann',
+  channelSecret: 'id',
 };
 
 const client = new line.Client(config);
