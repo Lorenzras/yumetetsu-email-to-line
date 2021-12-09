c6ba42a73956e744fc17684624b849e3c0eeecc1
Lorenz Ras <lenzras@gmail.com>
2021-12-09 11:16:42 +0900

    added logging

--- main.py
+++ main.py
@@ -1,26 +1,24 @@
 
+import logging
 from dotenv import load_dotenv
 from src.line.line_api import send_message
 from src.line.utils import getGroupIdByMailBox
-import os
+import logging
 import sys
 
+#Settings
+logging.basicConfig(filename='app.log', format='%(asctime)s - %(levelname)s - %(message)s', level=logging.DEBUG)
 load_dotenv()
 
-
-
 def main():
-    message = "問題が発生しました。管理者にお知らせください。"
-    mailbox = ""
-    print(len(sys.argv))
+    message = "メッセージはありませんでした。"
+    mailbox = "Nothing"
 
     if(len(sys.argv)>1):
         message = sys.argv[1]
         mailbox = sys.argv[2]
 
+    logging.debug(f"Mailbox : {mailbox}" )
     send_message(message,  getGroupIdByMailBox(mailbox))
-    #send_message("konnichiwa",  os.getenv('GROUP_ID_FUJISAWA'))
-
-
 if __name__ == "__main__":
   main()
--- src/line/line_api.py
+++ src/line/line_api.py
@@ -2,16 +2,20 @@ from linebot import LineBotApi
 from linebot.models import TextSendMessage
 from linebot.exceptions import LineBotApiError
 
+import logging
+logging.basicConfig(encoding="utf-8", filename='app.log', format='%(asctime)s %(name)s - %(levelname)s - %(message)s', level=logging.DEBUG)
+
 import os
 
 def send_message(msg, group_key):
   message = msg
   if(len(msg)>5000):
     message = msg[0:4999]
+
+
   line_bot_api = LineBotApi(os.getenv('CHANNEL_KEY'))
   try:
-      line_bot_api.push_message(group_key, TextSendMessage(text=message))
+      logging.debug(f"groupKey: {group_key}, totalLength: {len(msg)}, message: {message} ")
+      #line_bot_api.push_message(group_key, TextSendMessage(text=message))
   except LineBotApiError as e:
       print(e)
-
-  print("something")
